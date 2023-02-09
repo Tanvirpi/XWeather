984c9f3a0c8c82cb4f1fbbabf341e46ad60cbccb
tanvirpi <tanvir.pi@gmail.com>
2023-02-08 17:17:32 -0700

    searchrd city Json added

--- Assets/script.js
+++ Assets/script.js
@@ -10,33 +10,41 @@ const api = {
 
 buttonPress.addEventListener('click', setQuery);
 
+const numberOfDays = 5;
+
+const localStorageKey = 'previousSearches';
+
 function setQuery(evt) {
     evt.preventDefault();
     getResults(searchInput.value);
     console.log(searchInput.value);
-    var previousSearch = JSON.parse(localStorage.getItem("city")) || []
-    if (previousSearch.indexOf(searchInput.value) === -1) {
-        previousSearch.push(searchInput.value)
-        localStorage.setItem("city", JSON.stringify(previousSearch))
-        showSearchCity();
+    var previousSearch = JSON.parse(localStorage.getItem(localStorageKey)) || [];
+    
+    previousSearch.push(searchInput.value);
+    if (previousSearch.length > 10) {
+        previousSearch.shift();
     }
+    localStorage.setItem(localStorageKey, JSON.stringify(previousSearch))
+    showSearchCity();
 }
 
-showSearchCity();
-
 function showSearchCity() {
-    var previousSearch = JSON.parse(localStorage.getItem("city")) || []
+    var previousSearch = JSON.parse(localStorage.getItem(localStorageKey)) || [];
     var searchedCities = '';
     if (previousSearch.length > 10) {
         var n = 10;
-    } else { var n = previousSearch.length - 1 }
+    } else { 
+        var n = previousSearch.length - 1; 
+    }
     for (i = n; i >= 0; i--) {
-        searchedCities += `<button class="searchbtn1">${previousSearch[i]}</button>`;
+        searchedCities += 
+            `<button class="searchbtn1" onclick="getResults(this.innerHTML)">${previousSearch[i]}</button>`;
     }
     document.getElementById('searchedCity').innerHTML = searchedCities;
 }
 
 function getResults(query) {
+    // todo set city name
     const url = api.base + 'forecast?q=' + query + '&units=imperial&APPID=' + api.key;
     console.log(url);
     fetch(url)
@@ -48,35 +56,28 @@ function displayResults(data) {
     console.log(data);
     cityEl.innerText = searchInput.value;
 
-    for (i = 0; i < 6; i++) {
+    for (i = 0; i < numberOfDays; i++) {
         document.getElementById("temp" + (i + 1)).innerHTML = "Temp: " + Number(data.list[i].main.temp).toFixed(1) + "°F";
-
-    }
-
-    for (i = 0; i < 6; i++) {
         document.getElementById("humid" + (i + 1)).innerHTML = "Humidity: " + Number(data.list[i].main.humidity).toFixed(0) + "%";
-    }
-
-    for (i = 0; i < 6; i++) {
         document.getElementById("wind" + (i + 1)).innerHTML = "Wind: " + Number(data.list[i].wind.speed).toFixed(0) + "mph";
+        document.getElementById("img" + (i + 1)).src = "http://openweathermap.org/img/wn/" +
+            data.list[i].weather[0].icon + ".png";
     }
+}
 
+function showDate() {
+    var targetDate = new Date();
+    targetDate.setDate(targetDate.getDate());
+    
+    var dd = targetDate.getDate();
+    var mm = targetDate.getMonth() + 1;
+    var yyyy = targetDate.getFullYear();
+    
     for (i = 0; i < 6; i++) {
-        document.getElementById("img" + (i + 1)).src="http://openweathermap.org/img/wn/" +
-            data.list[i].weather[0].icon
-            + ".png";
+        const dayOfMonth = dd + i;
+        document.getElementById("date" + (i + 1)).innerHTML = mm + "/" + dayOfMonth + "/" + yyyy;
     }
 }
 
-var targetDate = new Date();
-targetDate.setDate(targetDate.getDate());
-
-var dd = targetDate.getDate();
-var mm = targetDate.getMonth() + 1;
-var yyyy = targetDate.getFullYear();
-
-for (i = 0; i < 6; i++) {
-    const dayOfMonth = dd + i;
-    document.getElementById("date" + (i + 1)).innerHTML = mm + "/" + dayOfMonth + "/" + yyyy;
-
-}
+showSearchCity();
+showDate();
